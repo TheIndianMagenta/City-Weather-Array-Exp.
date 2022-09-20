cf763fcb4f4289e3f40b4189346ef60ab42c6420
SamarthAnnigeri <Samarthgrand@gmail.com>
2022-09-20 13:15:51 -0700

    Initial Commit

---
+++ City Weather Array/City_Weather_ArrayApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_Weather_ArrayApp.swift
+//  City Weather Array
+//
+//  Created by Samarth Annigeri on 9/20/22.
+//
+
+import SwiftUI
+
+@main
+struct City_Weather_ArrayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
